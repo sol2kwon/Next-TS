3de34c3afa17a114eed29e6409a476a40f015a22
sol2kwon <amorfatisole@gmail.com>
2022-05-30 15:23:42 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import {Counter} from '@/components/counter/Couter'
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <Counter/>
+  )
+}
+
+export default Home
